72094a05e0bee8df79683f1c342d747502621d98
Juan Miguel Devilleros Ospina <juandevilleros@gmail.com>
2023-02-26 22:03:34 -0500

    Aun agregando estilos..

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,10 +3,10 @@ export default function Card(props) {
    return (
       <div className={styles.divBackground}>
          <div className={styles.divPortal}>
-            <button onClick={props.onClose}>X</button>
-            <h2>{props.name}</h2>
-            <h2>{props.species}</h2>
-            <h2>{props.gender}</h2>
+            <button type="button" class="btn btn-danger" className={styles.button} onClick={props.onClose}>X</button>
+            <h2 class="shadow-none p-3 mb-5 bg-light rounded">{props.name}</h2>
+            <h2 class="shadow-none p-3 mb-5 bg-light rounded">{props.species}</h2>
+            <h2 class="shadow-none p-3 mb-5 bg-light rounded">{props.gender}</h2>
             <img className={styles.image} src={props.image} alt="" />
          </div>
       </div>

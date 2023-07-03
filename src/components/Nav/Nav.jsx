1631b7048b2b5ea3db37c39c9b94f5544fc83e1b
Juan Miguel Devilleros Ospina <juandevilleros@gmail.com>
2023-07-03 17:10:13 -0500

    Cambios en Nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -8,10 +8,10 @@ class Nav extends React.Component{
         super()
     }
     render(){
-        return <nav className={styles.Nav}>
-            <Link to={"/home"}><button>Home</button></Link>
-            <Link to={"/about"}><button>About</button></Link>
-            <Link to={"/favorites"}><button>Favorites</button></Link>
+        return <nav className={styles.nav}>
+            <Link to={"/home"}><button className={styles.button}>Home</button></Link>
+            <Link to={"/about"}><button className={styles.button}>About</button></Link>
+            <Link to={"/favorites"}><button className={styles.button}>Favorites</button></Link>
             <SearchBar onSearch={this.props.onSearch}/>
             <hr/>
         </nav>
